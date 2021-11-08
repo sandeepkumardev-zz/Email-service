b8249521e2f7a5617a6def3f3ad48587bb681ef2
sandeep kumar <sandeepkumar@sandeeps-MacBook-Pro.local>
2021-11-08 12:24:08 +0530

    update model

--- models/user.model.go
+++ models/user.model.go
@@ -1,7 +1,8 @@
 package models
 
 type User struct {
-	ID    BinaryUUID `json:"id"`
-	Name  string     `json:"name"`
-	Email string     `json:"email"`
+	ID       BinaryUUID `json:"id"`
+	Name     string     `json:"name"`
+	Email    string     `json:"email"`
+	Password string     `json:"password"`
 }
